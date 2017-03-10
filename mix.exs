289ed492b484489b68926cb6418c72724acc39d4
Almas Akchabayev <almas@Almass-MacBook-Pro.local>
2017-03-11 01:26:56 +0600

    Add comments on libraries used

--- mix.exs
+++ mix.exs
@@ -17,17 +17,23 @@ defmodule ProductTracker.Mixfile do
      mod: {ProductTracker.Application, []}]
   end
 
+  @doc """
+  Include test/support/conn_case.ex when testing
+  """
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
-    [{:ecto, "~> 2.1"},
-     {:postgrex, "~> 0.13.2"},
-     {:httpoison, "~> 0.11.1"},
-     {:calendar, "~> 0.17.2"},
-     {:poison, "~> 3.1"}]
+    [{:ecto, "~> 2.1"}, # work with db
+     {:postgrex, "~> 0.13.2"}, # adapter for ecto
+     {:httpoison, "~> 0.11.1"}, # HTTP Client
+     {:calendar, "~> 0.17.2"}, # date-time conveniences
+     {:poison, "~> 3.1"}] # JSON handling
   end
 
+  @doc """
+  Ecto conveniences
+  """
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
